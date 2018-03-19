fa679ff24b078ac90a5dc6f78eb52dd6fa1f30f8
wydra76yan <yan@vydra.com>
2018-03-20 01:32:18 +0300

    Print title and description

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 const program = require('commander');
 const { prompt } = require('inquirer');
+
+program
+  .version('0.0.1')
+  .description('TODO app');
+
+// Craft questions to present to users
+const createQuestions = [
+  {
+    type : 'input',
+    name : 'title',
+    message : 'Enter title ...'
+  },
+  {
+    type : 'input',
+    name : 'description',
+    message : 'Enter description ...'
+  },
+];
+
+const todos = [];
+
+program
+  .command('create')
+  .alias('cr')
+  .description('Create new TODO item')
+  .action(() => {
+    prompt(createQuestions).then(answers => {
+      console.log(answers);
+    });
+  });
+
+
+program.parse(process.argv);
