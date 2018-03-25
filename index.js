c6fc638dda9c60581ee02e95b14de806d075d763
wydra76yan <yan@vydra.com>
2018-03-25 23:41:58 +0300

    Updated

--- index.js
+++ index.js
@@ -1,11 +1,77 @@
+
 const program = require('commander');
 const { prompt } = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
 
 program
   .version('0.0.1')
-  .description('TODO app');
+  .description('This is a TODO application');
 
-// Craft questions to present to users
+const STORAGE_PATH = path.resolve('./TODOs.json');
+const ACCOUNT_ID = 1;
+const { O_APPEND, O_RDONLY, O_CREAT } = fs.constants;
+
+//turn async func into promise
+const fsOpen = util.promisify(fs.open);
+const fsReadFile = util.promisify(fs.readFile);
+const fsWriteFile = util.promisify(fs.writeFile);
+
+
+function getAllTodos() {
+  return fsReadFile(STORAGE_PATH, { encoding: 'utf8', flag: O_RDONLY | O_CREAT })
+    .then((data) => {
+      let jsonText = data;
+      return JSON.parse(jsonText);
+    })
+    .then((storage) => {
+      return storage.todos;
+    });
+}
+
+function saveAllTodos(todos) {
+  return fsOpen(STORAGE_PATH, O_APPEND | O_CREAT)
+    .then(() => {
+      fsWriteFile(STORAGE_PATH, JSON.stringify({ todos }));
+    });
+}
+
+function guid() {
+  function s4() {
+    return Math.floor(( Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + '-' + s4();
+}
+
+function print(...args) {
+  console.info(...args);
+}
+
+function createTodo(data) {
+  return {
+    id: guid(),
+    ...data,
+    comment: null,
+    isLiked: false,
+  };
+}
+
+function createTodoItem(data) {
+   let todoId;
+  return getAllTodos()
+    .then((todos) => {
+      const todo = createTodo(data);
+      todoId = todo.id;
+      const result = [...todos, todo];
+      return saveAllTodos(result);
+    })
+    .then(() => todoId);
+}
+
+
 const createQuestions = [
   {
     type : 'input',
@@ -19,17 +85,17 @@ const createQuestions = [
   },
 ];
 
-const todos = [];
 
 program
   .command('create')
-  .alias('cr')
   .description('Create new TODO item')
   .action(() => {
-    prompt(createQuestions).then(answers => {
-      console.log(answers);
-    });
+    prompt(createQuestions)
+      .then(({ title, description }) => createTodoItem({ title, description }))
+      .then(print)
+      .catch((error) => {
+        throw error;
+      });
   });
-
 
 program.parse(process.argv);
